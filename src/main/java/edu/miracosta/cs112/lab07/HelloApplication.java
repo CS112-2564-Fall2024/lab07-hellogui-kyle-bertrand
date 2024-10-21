d2e012f0913b1b779237ca5eee1589e50f271314
Initial Commit <bertrandkyle1@gmail.com>
2024-10-21 14:39:16 -0700

    Initial commit.

--- src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
@@ -25,6 +25,7 @@ public class HelloApplication extends Application  { //inheriting core functiona
         StackPane layout = new StackPane();
 
         Scene scene = new Scene(layout, 400, 400);
+
         primaryStage.setScene(scene);
         primaryStage.show();
 
